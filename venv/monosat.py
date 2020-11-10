891c17a0b4027181f517d69cc206b64ebc511f5e
mspohrer <msporher@pdx.edu>
2020-11-10 12:55:09 -0800

    First commit: GIT IN THERE!!

---
+++ venv/monosat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def main():
+    # m is the number of monopoles
+    # ms is a list of the monopoles
+    # n is the number of rooms
+    # rs is the rooms
+    m = int(sys.argv[1])
+    n = int(sys.argv[2])
+
+    ms = range(1, m + 1)
+    rs = [[] for i in range(n)]
+
+
+if __name__ == '__main__':
+    main()
