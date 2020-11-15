553997666a0e1c806dab0f437e1455dd61ca8bd4
mspohrer <msporher@pdx.edu>
2020-11-15 11:40:45 -0800

    almost done

--- monosat.py
+++ monosat.py
@@ -10,24 +10,79 @@
 # by minisat.
 
 import sys
+import math
+
+# monos = number of monopoles, first arg passed by user
+# rooms = number of rooms, second arg passed by user
+monos = int(sys.argv[1])
+rooms = int(sys.argv[2])
+
+
+def one_room(clauses):
+    # creates the atoms responsible for ensuring no monopole can be
+    # rooms at once.
+    # clauses is a list of all clauses to be passed to miniSAT
+    # monos is the number of monopoles to be processed
+    # rooms is an upper bound to the number of rooms to be checked
+
+    not_clauses = []
+
+    for m in range(1, monos + 1):
+        clause = []
+        not_clause = []
+        for r in range(0, rooms):
+            clause.append((monos * r) + m)
+            not_clause.append(-((monos * r) + m))
+        clauses.append(clause)
+        not_clauses.append(not_clause)
+
+    for c in not_clauses:
+        clauses.append(c)
+
+
+def monopole(clauses):
+    # divides the monopoles so no two monopoles can be added
+    # together to equal a third in the same room
+    # clauses is a list of all clauses to be passed to miniSAT
+    # monos is the number of monopoles to be processed
+    # rooms is an upper bound to the number of rooms to be checked
+
+    if monos % 2 == 0:
+        mono_stop = monos//2
+    else:
+        mono_stop = math.ceil(monos/2)
+
+    print(mono_stop)
+    for m in range(1, mono_stop):
+        for r in range(0, rooms):
+            new_room_start = (monos * r) + m
+            new_room_stop = monos * r + monos - m + 1
+
+            print(m, "start=", new_room_start, "stop=", new_room_stop)
+            for i in range(new_room_start + 1, new_room_stop):
+                clauses.append([-new_room_start, -i, -(m+i)])
+
+
+def p_a(clauses):
+    for c in clauses:
+        print(*c, '0')
+
 
 def main():
-    # m: number of monopole
-    # n: number of rooms
-    m = int(sys.argv[1])
-    n = int(sys.argv[2])
+    clauses = []
+    num_atoms = monos * rooms
+    atoms = []
+    for r in range(0, rooms):
+        for m in range(1, monos + 1):
+            atoms.append((monos * r) + m)
 
-    ms = range(1, m + 1)
-    rs = [[] for i in range(n)]
-    todo = [*ms]
+    one_room(clauses)
+    monopole(clauses)
 
-    rs = dfs(rs, todo, n, 0)
-    if rs == 0:
-        print("unsat")
-    else:
-        pretty_print(rs)
-
-    check(rs)
+    print(*atoms)
+    print("p cnf", num_atoms, len(clauses))
+    #   p_a(clauses)
+    print("p cnf", num_atoms, len(clauses))
 
 
 if __name__ == '__main__':
